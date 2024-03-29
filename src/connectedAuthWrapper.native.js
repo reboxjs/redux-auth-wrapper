3e7cea6224b317419f4dfe60e037811bb0b35266
Muflah Nasir <muflahnasir@gmail.com>
2024-03-29 11:08:12 +0500

    added native auth wrapper

---
+++ src/connectedAuthWrapper.native.js
@@ -0,0 +1,28 @@
+import { connect } from "react-redux";
+import authWrapper from "./authWrapper.native";
+
+const connectedDefaults = {
+  authenticatingSelector: () => false,
+};
+
+export default (args) => {
+  const { authenticatedSelector, authenticatingSelector, preAuthAction } = {
+    ...connectedDefaults,
+    ...args,
+  };
+
+  return (DecoratedComponent) =>
+    connect(
+      (state, ownProps) => ({
+        isAuthenticated: authenticatedSelector(state, ownProps),
+        isAuthenticating: authenticatingSelector(state, ownProps),
+      }),
+      (dispatch) => ({
+        preAuthAction: () => {
+          if (preAuthAction) {
+            dispatch(preAuthAction());
+          }
+        },
+      })
+    )(authWrapper(args)(DecoratedComponent));
+};
